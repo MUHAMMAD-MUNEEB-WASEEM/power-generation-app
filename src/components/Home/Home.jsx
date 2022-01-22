692616744809e5d0164cb72c0a89d01c1e04e1fa
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2022-01-22 12:46:27 +0500

    initial commit with home page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import './Home.css';
+
+function Home() {
+
+  return (
+    
+        <div className="home">
+            <div className='home__container'>
+                <div className='home__heading'>
+                    <h1>LOAD PROFILE ANALYSIS</h1>
+                </div>
+                <div className='home__button'>
+                    <button>Click to Select Load Profile</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
